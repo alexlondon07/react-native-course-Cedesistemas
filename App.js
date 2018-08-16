6a3224b750a24c3ac16fce6da5e78cc143496513
Alexander Londoño Espejo <alexlondon07@gmail.com>
2018-08-15 21:10:03 -0500

    .

--- App.js
+++ App.js
@@ -1,19 +1,40 @@
 import React, {Component} from 'react';
 import { 
-  StyleSheet,
-  Text, 
-  View,
-  Image,
-  TextInput,
-  Switch,
-  Button,
-  ScrollView,
   AlertIOS
 } from 'react-native';
 
+import Profile from './src/scenes/Profile/index';
+import ProfileOverView from './src/scenes/Profile/components/profile-overview';
+import FollowOverView from './src/scenes/Profile/components/follow-overview';
+import FormOverView from './src/scenes/Profile/components/form-overview';
+
 type Props = {};
 export default class App extends Component<Props> {
-  
+
+  constructor(props){
+    super(props);
+    this.state ={
+      profile: {}
+    }
+  }
+
+  //Método de Cilo de Vida
+  componentDidMount (){
+    const profile = {
+      name: 'Alexander Londoño Espejo',
+      location: 'Caucasia - Ant',
+      image: 'https://pbs.twimg.com/profile_images/939327802746208256/i2GEJvdF_400x400.jpg',
+      followers: 45678,
+      followings: 789008,
+      phone: "3338880",
+      twitter: '@alexlondon07'
+    };
+    this.setState({
+      profile
+      //profile: profile
+    })
+  }
+
   showAlert(){
     AlertIOS.alert( 
       'Alert Title', 
@@ -21,118 +42,18 @@ export default class App extends Component<Props> {
     );
   }
 
-  
+  changeText = text => {
+    console.log('changeText');
+    this.setState({ profile:{ ... this.state.profile, name: text } });
+  }
+
   render() {
     return (
-      <ScrollView>
-
-        <View style={styles.profileContainer}>
-            <Text style={styles.profileTittle}> View Profile </Text>
-            <Image
-              source = { { uri: 'https://pbs.twimg.com/profile_images/939327802746208256/i2GEJvdF_400x400.jpg' } }
-              style={styles.profileImage}
-            />
-            <Text style={styles.profileName}> Alexander Andrés </Text>
-            <Text style={styles.profileLocation}> Medellín - Colombia </Text>
-        </View>
-
-        <View style={styles.followContainer}>
-            <View style={ styles.followWrapper}>
-              <Text style={ styles.followValue }> 90,000 </Text>
-              <Text style={ styles.followTittle }> Followers </Text>
-            </View>
-            <View style={ styles.followWrapper}>
-              <Text style={ styles.followValue }> 10,000 </Text>
-              <Text style={ styles.followTittle }> Followings </Text>
-            </View>
-        </View>
-
-        <View style={ styles.formWrapper }>
-          <Text style={ styles.followLabel }> Name </Text>
-          <TextInput
-            placeholder = 'Name field is required'
-            style = { styles.formInput }
-           />
-          <Text style={ styles.followLabel }> @Twitter </Text>
-          <TextInput
-            placeholder = 'Twitter name field is required'
-            style = { styles.formInput }
-           />
-          <Text style={ styles.followLabel }> Phone </Text>
-          <TextInput
-            placeholder = 'Phone field is required'
-            style = { styles.formInput }
-           />
-            <Text  style={ styles.followLabel } >All Share Social Data </Text>
-            <Switch
-              onTintColor={ '#3949AB' } 
-              value={ true } 
-            /> 
-            <Button
-                onPress = { this.showAlert }
-                color = { "#3949AB" }
-                title = { 1 == 1 ? "Sync with facebook": "no show nothing"} 
-            /> 
-
-        </View>
-        
-      </ScrollView>
+      <Profile>
+        <ProfileOverView profile={ this.state.profile } />
+        <FollowOverView followings={ this.state.profile.followings } followers={ this.state.profile.followers }/>
+        <FormOverView onPress={ this.showAlert } profile={ this.state.profile } onChangeText = { this.changeText }/>
+      </Profile>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  profileContainer: {
-    backgroundColor: '#3949AB',
-    justifyContent: 'center',
-    alignItems: 'center'
-  },
-  profileTittle:{
-    color: '#fff',
-    fontSize: 30,
-    marginTop: 10
-  },
-  profileName:{
-    color: '#fff',
-    fontSize: 24,
-    marginTop: 20
-  },
-  profileImage:{
-    width: 140,
-    height: 140,
-    borderRadius: 70,
-    resizeMode: 'cover'
-  },
-  profileLocation:{
-    color: '#fff',
-    padding: 10
-  },
-  followContainer:{
-    backgroundColor: '#1976D2',
-    flexDirection: 'row'
-  },
-  followTittle: {
-    color: '#fff',
-    fontSize: 18,
-  },
-  followValue: {
-    color: '#fff',
-    fontSize: 20,
-  },
-  followWrapper:{
-    flex: 1,
-    alignItems: 'center',
-    padding: 10
-  },
-  formWrapper:{
-    marginHorizontal: 25
-  },
-  followLabel:{
-    color: '#3F51B5',
-    marginVertical: 10
-  },
-  formInput: {
-    borderBottomWidth: 1,
-    borderBottomColor: '#ccc'
-  }
-});
